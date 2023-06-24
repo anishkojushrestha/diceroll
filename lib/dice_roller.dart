3fe85d72f90bf6b749b3c1060ededfff4dbf2a25
awneesh shrestha <87461543+anishkojushrestha@users.noreply.github.com>
2023-06-24 08:44:02 +0545

    commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var currentDiceRoll = 2;
+  void rollDices() {
+    setState(() {
+      currentDiceRoll = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      //center the column vertically
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          'assets/images/dice-$currentDiceRoll.png',
+          width: 200,
+        ),
+        const SizedBox(
+          height: 15,
+        ),
+        TextButton(
+          onPressed: rollDices,
+          style: TextButton.styleFrom(
+            // padding: const EdgeInsets.only(top: 20),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 28),
+          ), //foregroundcolor like a text color
+          child: const Text('Roll Dices'),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'gardient_container.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: const GradientContainer(),
+      ),
+    ),
+  );
+}
